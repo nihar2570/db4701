ebc313e939817d975cdb140cdee4c1b0d183ceb6
Daljit <sharma.dal@gmail.com>
2016-12-08 02:27:44 -0500

    blah

--- login.php
+++ login.php
@@ -7,7 +7,7 @@ $password = ($_POST['pass']);
 $result = mysqli_query($con,"SELECT * FROM Customers WHERE CustomerID='$username'");
 //$result = mysqli_query($con,"SELECT * FROM customers WHERE ContactName='$username' AND password='$password'");
 //$result = mysqli_query($con,"SELECT * FROM users WHERE name='$username' AND password='$password'");
-if(!empty($result))
+if(mysqli_num_rows($result)!=0)
 {
 	//sets some equivalent to global variiables
 	$_SESSION["username"] = $username;
@@ -18,10 +18,8 @@ if(!empty($result))
 
 	}
 else{
-	echo "No such user exists";
+	echo " no such user exists";
 }
-if(!empty($result)){
 mysqli_free_result($result);
-}
 mysqli_close($con);
 ?>
